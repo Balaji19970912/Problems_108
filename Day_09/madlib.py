3b29910c1a7d7bec6014b9ac29444cb506b1f326
Balaji S Tanganoor <36842756+Balaji19970912@users.noreply.github.com>
2024-02-13 15:07:34 +0530

    madlib.py

--- Day_09/madlib.py
+++ Day_09/madlib.py
@@ -1,1 +1,34 @@
-print('Madlib Code')
+import string
+with open("story.txt", "r") as f:
+    story = f.read()
+    # print(story)
+
+# words = [] # lists
+words = set()  # set
+start_of_word = -1
+
+target_start = "["
+target_end = "]"
+
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i
+
+    if char == target_end and start_of_word != -1:
+        word = story[start_of_word: i+1]
+        # words.append(word) # list if we start go with the append
+        words.add(word)  # add with the set
+        start_of_word = -1
+print(words)
+
+answers = {}
+
+for word in words:
+    answer = input("Enter the answer for the word : " + word+" : ")
+    answers[word] = answer
+
+print(answers)
+
+for word in words:
+    story = story.replace(word, answers[word])
+print(story)
